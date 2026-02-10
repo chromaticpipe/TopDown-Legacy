ac7150455b1837575b41adf88194abccea238b47
PAS <pasrc@proton.me>
2026-02-10 15:54:26 -0500

    extra }

--- src/p_mobj.c
+++ src/p_mobj.c
@@ -9016,7 +9016,6 @@ mobj_t *P_SpawnMobj(fixed_t x, fixed_t y, fixed_t z, mobjtype_t type)
 			)
 			P_SpawnShadowMobj(mobj);
 	}
-	}
 
 	if (!(mobj->flags & MF_NOTHINK))
 		P_AddThinker(&mobj->thinker);
